d8031303d8eb16e05a412d79b880bd94f82ec8e6
stephan14 <zsf657135507@gmail.com>
2016-09-09 10:15:11 +0800

    实现Singleton模式

--- other/Offer/p2.cpp
+++ other/Offer/p2.cpp
@@ -39,8 +39,9 @@ Singleton* Singleton::Instantialize()
     {   
         //double check  
         Lock lock(cs);          
-        //用lock实现线程安全，用资源管理类，实现异常安全  
-        //使用资源管理类，在抛出异常的时候，资源管理类对象会被析构，析构总是发生的无论是因为异常抛出还是语句块结束。  
+        //用lock实现线程安全，用资源管理类实现异常安全  
+        //使用资源管理类，在抛出异常的时候，资源管理类对象会被析构，
+        //析构总是发生的无论是因为异常抛出还是语句块结束。  
         if( pInstance == NULL )  
         {  
             pInstance = new Singleton();  
