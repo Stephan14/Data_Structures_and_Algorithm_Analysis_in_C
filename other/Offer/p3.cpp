58d6a823848b564df7201d3e7241e1cf16d89840
stephan14 <zsf657135507@gmail.com>
2016-09-10 16:04:23 +0800

    二维数组中查找

--- other/Offer/p3.cpp
+++ other/Offer/p3.cpp
@@ -1,9 +1,15 @@
-ass Solution {
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+class Solution {
 public:
-    bool Find( vector<vector<int> > array,int target )
+    bool findTarget( vector<vector<int> > array,int target )
     {
             int row = 0;
-            int cloumn = array[0].size() - 1;
+            int cloumn = array[ 0 ].size() - 1;
 
             while(  row < array.size() && cloumn >= 0 )
             {
@@ -18,3 +24,36 @@ public:
             return false;
     }
 };
+
+int main()
+{
+    Solution s;
+    int m[][ 4 ] = {
+        { 1, 2, 8, 9 },
+        { 2, 4, 9, 12 },
+        { 4, 7, 10, 13 },
+        { 6, 8, 11, 15 },
+    };
+        
+    vector< int > v1( &m[ 0 ][ 0 ], &m[ 0 ][ 0 ] + 4 ); 
+    vector< int > v2( &m[ 1 ][ 0 ], &m[ 1 ][ 0 ] + 4 );
+    vector< int > v3( &m[ 2 ][ 0 ], &m[ 2 ][ 0 ] + 4);
+    vector< int > v4( &m[ 3 ][ 0 ], &m[ 3 ][ 0 ] + 4);
+
+    vector< vector<int> > v;
+    v.push_back( v1 );
+    v.push_back( v2 );
+    v.push_back( v3 );
+    v.push_back( v4 );
+
+    cout << v[0].size() << endl;
+    if( s.findTarget( v, 15  ) )
+    {
+        cout<< "存在" << endl;
+    }
+    else
+    {
+        cout<< "不存在" << endl;
+    }
+    return 0;
+}
