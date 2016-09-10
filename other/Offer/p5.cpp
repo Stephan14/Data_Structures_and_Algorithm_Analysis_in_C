61d8bd3ccd0093adea818bc3e0492a370f53ef37
stephan14 <zsf657135507@gmail.com>
2016-09-10 18:01:57 +0800

    从未到头打印链表

--- other/Offer/p5.cpp
+++ other/Offer/p5.cpp
@@ -9,6 +9,66 @@ struct Node
     struct Node *next;
 };
 
+
+void addToTail( struct Node ** listHead, int value )
+{
+    struct Node *tempNode = new Node();
+    tempNode->value = value;
+    tempNode->next  = NULL;
+    
+    //插入的节点为头结点
+    if( *listHead == NULL  )
+        *listHead = tempNode;
+    else
+    {
+        struct Node *tempHead = *listHead;
+        while( tempHead->next != NULL )
+            tempHead = tempHead->next;
+
+        tempHead->next = tempNode;
+    }
+
+}
+
+void removeNode( struct Node **listHead, int value )
+{
+    if( listHead == NULL || *listHead == NULL )
+        return;
+
+    struct Node *toBeDeleted = NULL;
+    //删除的节点为头结点
+    if( ( *listHead )->value == value )
+    {
+        toBeDeleted = *listHead;
+        *listHead = ( *listHead )->next;        
+    }    
+    else
+    {
+        struct Node * tempHead = *listHead;
+        //直接从第二个节点开始判断   
+        while( tempHead->next != NULL && tempHead->next->value != value )
+            tempHead = tempHead->next;
+        if( tempHead->next != NULL && tempHead->next->value == value )
+        {
+            toBeDeleted = tempHead->next;
+            tempHead->next = tempHead->next->next;            
+        }
+        if( toBeDeleted != NULL )
+        {
+            delete toBeDeleted;
+            toBeDeleted = NULL;
+        }
+    }
+}
+
+void printList( struct Node *list )
+{
+    while( list != NULL )
+    {
+        cout << list->value << "  " << endl;
+        list = list->next;
+    }
+}
 void printListReversinglyIteratively( struct Node * listHead )
 {
     stack<struct Node *> nodes;
@@ -22,7 +82,7 @@ void printListReversinglyIteratively( struct Node * listHead )
 
     while( !nodes.empty() )
     {
-        pNode = nodes.top;
+        pNode = nodes.top();
         cout << pNode->value;
         nodes.pop();
     }
@@ -44,5 +104,18 @@ void printListReversinglyRecursively( struct Node *listHead )
 
 int main()
 {
+    struct Node *head = NULL;
+    addToTail( &head, 3 );
+    addToTail( &head, 23 );
+    addToTail( &head, 1 );
+    addToTail( &head, 8 );
+
+    printList( head );
+    cout << "倒序输出" << endl;
+    printListReversinglyIteratively( head );
+    removeNode( &head, 3 );
+    removeNode( &head, 23 );
+    removeNode( &head, 1 );
+    removeNode( &head, 8 );
     return 0;
 }
